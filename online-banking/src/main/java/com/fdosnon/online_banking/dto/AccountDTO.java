6817fa88cde826a8f875721259ea46f3f6ea3c62
fleurdosnon <fleurdosnon@gmail.com>
2024-06-17 12:50:32 -0400

    Added field balance to AccountDTO

--- online-banking/src/main/java/com/fdosnon/online_banking/dto/AccountDTO.java
+++ online-banking/src/main/java/com/fdosnon/online_banking/dto/AccountDTO.java
@@ -2,9 +2,12 @@ package com.fdosnon.online_banking.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class AccountDTO {
     Integer id;
     String number;
     String type;
+    BigDecimal balance;
 }
--- online-banking/src/test/java/com/fdosnon/online_banking/mappers/AccountMapperTest.java
+++ online-banking/src/test/java/com/fdosnon/online_banking/mappers/AccountMapperTest.java
@@ -5,6 +5,8 @@ import com.fdosnon.online_banking.entities.Account;
 import com.fdosnon.online_banking.util.AccountType;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,6 +19,7 @@ class AccountMapperTest {
         account.setId(33);
         account.setNumber("123456");
         account.setType(AccountType.CHEQUE);
+        account.setBalance(BigDecimal.valueOf(350.35));
         // WHEN
         AccountDTO result = AccountMapper.INSTANCE.accountToAccountDTO(account);
         // THEN
@@ -24,5 +27,6 @@ class AccountMapperTest {
         assertEquals(account.getId(), result.getId());
         assertEquals(account.getNumber(), result.getNumber());
         assertEquals("CHEQUE", result.getType());
+        assertEquals(account.getBalance(), result.getBalance());
     }
 }
